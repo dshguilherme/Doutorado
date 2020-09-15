990a2a401ee01cb4e004e68e00e7aa642173a231
Guilherme H. da Silva <dshguilherme@gmail.com>
2020-09-15 14:47:51 -0300

    Updating the Schwarz Method
    Now it Iterates!
    Doesn't give the correct answer tho

--- FEniCS Scripts/SCHWARZ_OverlappingMembraneEigenValueProblem.py
+++ FEniCS Scripts/SCHWARZ_OverlappingMembraneEigenValueProblem.py
@@ -34,10 +34,10 @@ bc_D.mark(omega_markers, 0)
  # Omega 1       
 class DirichletBoundaryOmega1(SubDomain):
     def inside(self,x,on_boundary):
-        return on_boundary and (x[0] < b+1.5*o -DOLFIN_EPS)
+        return on_boundary and (x[0] < b +1.5*o)
 class RobinBoundaryOmega1(SubDomain):
     def inside(self,x,on_boundary):
-        return on_boundary and (x[0] > b+1.5*o -DOLFIN_EPS)
+        return on_boundary and (x[0] >= b+1.5*o -DOLFIN_EPS)
  # Boundary Markers
 omega1_markers = MeshFunction('size_t', omega1, omega1.topology().dim()-1,0)
 omega1_markers.set_all(9999)
@@ -49,7 +49,7 @@ bc_R1.mark(omega1_markers, 1)
  # Omega 2
 class DirichletBoundaryOmega2(SubDomain):
     def inside(self,x,on_boundary):
-        return on_boundary and (x[0] > b+DOLFIN_EPS)
+        return on_boundary and (x[0] > b)
 class RobinBoundaryOmega2(SubDomain):
     def inside(self,x,on_boundary):
         return on_boundary and (x[0] < b+DOLFIN_EPS)
@@ -167,48 +167,86 @@ vtkfile << uu2
 
 
 ## Schwarz Alternating Algorithm
-# First Step: Do F1 = du2/dn2 G1 = u2
-W2 = VectorFunctionSpace(omega2, 'P', V2.ufl_element().degree())
-grad_u2 = project(grad(uu2), W2)
-print(grad_u2(0.8,0.8)[0], grad_u2(0.8,0.8)[1])
-print(uu2(0.8,0.8))
+iteration = 0
+while (iteration < 11):
+    # First Step: Do F1 = du2/dn2 G1 = u2
+    V1 = FunctionSpace(omega1,"Lagrange", 1)
+    V2 = FunctionSpace(omega2, "Lagrange",1)
 
-u1 = TrialFunction(V1)
-v1 = TestFunction(V1)
+    g1 = Function(V1)
+    LagrangeInterpolator.interpolate(uu2,g1)
+    f1 = Function(V1)
 
-dx = Measure('dx', domain = omega1)
-ds = Measure('ds', subdomain_data = omega1_markers)
+    ff2 = project(uu2.dx(0) +uu2.dx(1),V2)
+    LagrangeInterpolator.interpolate(ff2,f1)
 
-class OmegaF1(UserExpression):
-    def eval(self, value, x):
-        f = grad_u2(x[0], x[1])
-        value[0] = f[0]
-        value[1] = f[1]
-class OmegaG1(UserExpression):
-    def eval(self, value, x):
-        g = uu2(x[0], x[1])
-        value[0] = g[0]
+    u1 = TrialFunction(V1)
+    v1 = TestFunction(V1)
 
-f1 = OmegaF1()
-g1 = OmegaG1()
-n = FacetNormal(omega1)
+    ## Solve for u1
+    dx = Measure('dx', domain = omega1)
+    ds = Measure('ds', subdomain_data = omega1_markers)
+    ak1 = inner(grad(u1),grad(v1))*dx -(1-g1)*(u1.dx(0) +u1.dx(1))*v1*ds(1) -f1*u1*v1*ds(1)
+    am1 = u1*v1*dx
 
-ak1 = inner(grad(u1),grad(v1))*dx -(1-g1)*inner(grad(u1),n)*v1*ds(1) -f1[0]*u1*v1*ds(1) -f1[1]*u1*v1*ds(1)
-am1 = u1*v1*dx
+    K1 = PETScMatrix()
+    bcs1 = DirichletBC(V1, 0.0, bc_D1)
+    assemble(ak1, tensor=K1)
+    bcs1.apply(K1)
 
-K1 = PETScMatrix()
-bcs1 = DirichletBC(V1, 0.0, bc_D1)
-bcs1.apply(K1)
-assemble(ak1, tensor=K1)
-M1 = PETScMatrix()
-assemble(am1, tensor=M1)
+    M1 = PETScMatrix()
+    assemble(am1, tensor=M1)
 
-omega1solver = SLEPcEigenSolver(K1,M1)
-omega1solver.parameters['spectrum'] = "smallest magnitude"
-omega1solver.parameters['tolerance'] = 1e-6
-omega1solver.parameters['problem_type'] = "pos_gen_non_hermitian"
-omega1solver.solve(1)
-r1, c1, rx1, cx1 = omega1solver.get_eigenpair(0)
-print("First Iteration Omega 1 Frequency: ", sqrt(r1))
+    omega1solver = SLEPcEigenSolver(K1,M1)
+    omega1solver.parameters['spectrum'] = "smallest magnitude"
+    omega1solver.parameters['tolerance'] = 1e-6
+    omega1solver.parameters['problem_type'] = "pos_gen_non_hermitian"
+    omega1solver.solve(0)
+    r1, c1, rx1, cx1 = omega1solver.get_eigenpair(0)
+    print("Omega 1 Frequency: ", sqrt(r1))
 
-        
+    uu1 = Function(V1)
+    uu1.vector()[:] = rx1
+    plot(uu1)
+    vtkfile1 = File('SchwarzMembraneEigenvalueProblem/omega1_iter1.pvd')
+    vtkfile1 << uu1
+
+    ## Do F1 = du1/dn1 G2 = u1
+
+    g2 = Function(V2)
+    LagrangeInterpolator.interpolate(uu1,g2)
+    f2 = Function(V2)
+    ff1 = project(uu1.dx(0)+uu1.dx(1),V1)
+    LagrangeInterpolator.interpolate(ff1,f2)
+
+    u2 = TrialFunction(V2)
+    v2 = TestFunction(V2)
+
+    ## Solve for u2
+    dx = Measure('dx', domain = omega2)
+    ds = Measure('ds', subdomain_data = omega2_markers)
+    ak2 = inner(grad(u2),grad(v2))*dx -(1-g2)*(u2.dx(0) +u2.dx(1))*v2*ds(1) +f2*u2*v2*ds(1)
+    am2 = u2*v2*dx
+
+    K2 = PETScMatrix()
+    bcs2 = DirichletBC(V2, 0.0, bc_D2)
+    assemble(ak2, tensor=K2)
+    bcs2.apply(K2)
+
+    M2 = PETScMatrix()
+    assemble(am2, tensor=M2)
+
+    omega2solver = SLEPcEigenSolver(K2,M2)
+    omega2solver.parameters['spectrum'] = "smallest magnitude"
+    omega2solver.parameters['tolerance'] = 1e-6
+    omega2solver.parameters['problem_type'] = "pos_gen_non_hermitian"
+    omega2solver.solve(0)
+    r2, c2, rx2, cx2 = omega2solver.get_eigenpair(0)
+    print("Omega 2 Frequency: ", sqrt(r2))
+    iteration += 1
+
+    uu2 = Function(V2)
+    uu2.vector()[:] = rx2
+    plot(uu2)
+    vtkfile1 = File('SchwarzMembraneEigenvalueProblem/omega2_iter1.pvd')
+    vtkfile1 << uu2
