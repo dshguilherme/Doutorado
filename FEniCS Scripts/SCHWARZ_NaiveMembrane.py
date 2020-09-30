631d88bfa973339b28d9d8aa231b50b3be7d9a1e
Guilherme H. da Silva <dshguilherme@gmail.com>
2020-09-30 10:14:13 -0300

    Updated Naive with L2 error norm
    L2 error calculation and loop stoppage

--- FEniCS Scripts/SCHWARZ_NaiveMembrane.py
+++ FEniCS Scripts/SCHWARZ_NaiveMembrane.py
@@ -2,7 +2,7 @@ from dolfin import *
 import mshr
 
 def schwarz_membrane_solver(L,h,o, mesh_resolution,number_of_refinements,
-                             max_iterations):
+                             max_iterations, tolerance):
     b = L*(1-o)/2 # Non Overlapping Side of the Rectangle
 
     # Mesh
@@ -172,10 +172,22 @@ def schwarz_membrane_solver(L,h,o, mesh_resolution,number_of_refinements,
                                     omega_right.topology().dim()-1)
     right_robin = RobinRight()
     right_robin.mark(right_boundaries, 1)
+    
+    # Defining the Overlap Domain
+    overlap_left = MeshFunction("size_t", omega_left,
+                                 omega_left.topology().dim())
+    overlap.mark(overlap_left,1)
+
+    overlap_right = MeshFunction("size_t", omega_right,
+                                 omega_right.topology().dim())
+    overlap.mark(overlap_right,1)
 
     # iterate
     iter = 0
-    while(iter < max_iterations):
+    L2_error = 9999
+    smallest = 9999
+    the_iter = 0
+    while((iter < max_iterations)):
         # First Step: Do F1 = du/dn2, G1 = u2
         uu2.set_allow_extrapolation(True)
         f1 = project(uu2.dx(1) -uu2.dx(0), V=V1)
@@ -206,6 +218,20 @@ def schwarz_membrane_solver(L,h,o, mesh_resolution,number_of_refinements,
         uu1.vector()[:] = rx1
         vtkfile1 = File('SchwarzMembraneNaive/left' + str(iter) + '.pvd')
         vtkfile1 << uu1
+        
+        # Step 2.5: L2 norm on the overlap
+        dxo = Measure('dx', subdomain_data=overlap_left)
+        error = ((uu1-g1)**2)*dxo(1)
+        L2_error_left = sqrt(abs(assemble(error)))
+        print("L2 error: ", L2_error_left)
+        L2_error = L2_error_left
+        if (L2_error < tolerance):
+            print("Process has converged on iteration ", iter, "on the left domain")
+            break
+        if (smallest > L2_error):
+            smallest = L2_error
+            the_iter = iter
+
 
         # Step 3: Do F2 = du/dn1, G2 = u1
         uu1.set_allow_extrapolation(True)
@@ -215,7 +241,7 @@ def schwarz_membrane_solver(L,h,o, mesh_resolution,number_of_refinements,
         u2 = TrialFunction(V2)
         v2 = TestFunction(V2)
 
-        # Step 2: Solve for u2
+        # Step 4: Solve for u2
         ds = Measure('ds', subdomain_data=right_boundaries)
 
         k2 = inner(grad(u2),grad(v2))*dx -(1-g2)*(+u2.dx(0) -u2.dx(1))*v2*ds(1) -f2*u2*v2*ds(1)
@@ -237,7 +263,27 @@ def schwarz_membrane_solver(L,h,o, mesh_resolution,number_of_refinements,
         uu2.vector()[:] = rx2
         vtkfile2 = File('SchwarzMembraneNaive/right' + str(iter) +'.pvd')
         vtkfile2 << uu2
-        iter += 1
 
-schwarz_membrane_solver(L=1.5, h=1.0, o=0.1, mesh_resolution=10,
-                         number_of_refinements=1, max_iterations=2)
+        # Step 4.5: L2 norm on the overlap
+        dxo = Measure('dx', subdomain_data=overlap_right)
+        error = ((uu2-g2)**2)*dxo(1)
+        L2_error_right = sqrt(abs(assemble(error)))
+        print("L2 error: ", L2_error_right)
+        L2_error = L2_error_right
+        if (L2_error < tolerance):
+            print("Process has converged on iteration ", iter, "on the right domain" )
+            break
+        if (smallest > L2_error):
+            smallest = L2_error
+            the_iter = iter
+        iter += 1
+    if (iter>= max_iterations):
+        print("Process has not converged to tolerance")
+        print("L2 error of last iteration:", L2_error)
+        print("Smallest L2 error:", smallest)
+        print("Achieved in iteration:", the_iter)
+
+
+schwarz_membrane_solver(L=1.5, h=1.0, o=0.2, mesh_resolution=50,
+                         number_of_refinements=2, max_iterations=10,
+                         tolerance=1e-3)
